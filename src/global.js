d75295783bb7f340d49f463e9b8a86dac82825bd
vinithaneyashree <vinithaneyashree2901@gmail.com>
2022-12-12 15:51:55 +0530

    final commit

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 // export const API_URL ="http://localhost:8000";
-export const API_URL = "https://blog-app-serv.herokuapp.com";
+export const API_URL = "https://blog-app-server-0r66.onrender.com";
 // export const API_URL ="http://localhost:8000";
